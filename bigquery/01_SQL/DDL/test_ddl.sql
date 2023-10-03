d9ff535b02092604996409469f267ea4a98b86bf
VPATNM1 <manideep.patnam@techolution.com>
2023-10-03 23:07:11 +0530

    06

--- bigquery/01_SQL/DDL/test_ddl.sql
+++ bigquery/01_SQL/DDL/test_ddl.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE PROCEDURE `synthetic-eon-241312.test_stored_procedure.CREATE_TABLE_DDL05`()
+CREATE OR REPLACE PROCEDURE `synthetic-eon-241312.test_stored_procedure.CREATE_TABLE_DDL06`()
 BEGIN
   CREATE OR REPLACE TABLE test_stored_procedure.sample_table (
     id INT64,
